d95e64ee41cbad3e1e97fce5bdfbf5b6df071b58
Wouter Reijgersberg <wouter.reijgersberg@student.uantwerpen.be>
2021-09-23 10:27:10 +0200

    Added Cart functionality

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+	selector: 'cart',
+	templateUrl: 'cart.component.html',
+	styleUrls: ['./cart.component.css']
+})
+
+export class CartComponent {
+
+	items = this.cartService.getItems();
+
+	constructor(
+		private cartService: CartService
+	){}
+
+}
